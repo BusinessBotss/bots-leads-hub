c276e092eca4fdb3e15c5d51683d23465874b8b6
BusinessBotss <businessbots.info@gmail.com>
2025-05-06 21:32:11 +0200

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,19 @@
-<form method="post">
-  <label>
-    Name:
-    <input type="text" name="name" required />
-  </label>
-  <label>
-    Email:
-    <input type="email" name="email" required />
-  </label>
-  <label>
-    Message:
-    <textarea name="message" />
-  </label>
-  <button type="submit">Submit</button>
-</form>
+export default function ContactForm() {
+  return (
+    <form method="post">
+      <label>
+        Name:
+        <input type="text" name="name" required />
+      </label>
+      <label>
+        Email:
+        <input type="email" name="email" required />
+      </label>
+      <label>
+        Message:
+        <textarea name="message" />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
