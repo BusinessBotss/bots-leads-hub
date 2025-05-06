65e3af1fa05ff80dab4504e62c17ee85fc3d0dbd
BusinessBotss <businessbots.info@gmail.com>
2025-05-06 17:27:55 +0200

    Create thank-you.tsx

---
+++ thank-you.tsx
@@ -0,0 +1,8 @@
+export default function ThankYouPage() {
+  return (
+    <div>
+      <h1>Thank you!</h1>
+      <p>We’ve received your message. A member of the Business Bots team will contact you shortly via email or WhatsApp.</p>
+    </div>
+  );
+}
